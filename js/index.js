2228d170e1e07a8460e9be94601431b5a75c67cf
Luan <jadsonluan1@gmail.com>
2017-11-26 20:28:02 -0300

    Adicionado sol. Adicionado planeta que orbita o sol.

--- js/index.js
+++ js/index.js
@@ -7,29 +7,42 @@ function init() {
 	scene = new THREE.Scene();
 	
 	camera = new THREE.PerspectiveCamera(75, WIDTH / HEIGHT, 0.1, 1000);
-	camera.position.z = 100;
+	camera.position.z = 200;
+	camera.position.y = 200;
+	camera.lookAt(scene.position);
 
 	renderer = new THREE.WebGLRenderer();
 	renderer.setSize(WIDTH, HEIGHT);
+	renderer.setClearColor(0x10171D, 1);
 	document.getElementById("container").appendChild(renderer.domElement);
 }
 
+var origin = new THREE.Vector3(0,0,0);
+
+function orbit(planet, radius, speed, t) {
+	variation = (t * speed) / 250;
+	planet.position.x = Math.cos(variation) * 100;
+	planet.position.z = Math.sin(variation) * 100;
+}
+
 function animate(t) {
+	orbit(sphere, 100, 0.2, t);
 	requestAnimationFrame(animate);
 	renderer.render(scene, camera);
 }
 
 function createObjects() {
-	sphereGeo = new THREE.SphereGeometry(5, 32, 32);
-	sphereMat = new THREE.MeshBasicMaterial({color: 0x00FF00});
+	var sphereGeo = new THREE.SphereGeometry(10, 32, 32);
+	var sphereMat = new THREE.MeshBasicMaterial({color: 0x0000F0});
 	sphere = new THREE.Mesh(sphereGeo, sphereMat);
 	scene.add(sphere);
+
+	var sunGeo = new THREE.SphereGeometry(25, 32, 32);
+	var sunMat = new THREE.MeshBasicMaterial({color: 0xFFFF00});
+	var sun = new THREE.Mesh(sunGeo, sunMat);
+	scene.add(sun);
 }
 
-
 init();
-
-// do stuff
 createObjects();
-
 animate(new Date().getTime());
